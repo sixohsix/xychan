aeefee87283fe9ba383a76cde64ae65e9f6538db
Mike Verdone <mike.verdone@gmail.com>
2010-10-15 00:30:44 +0200

    Improve title of atom feed.

--- xychan/atom.py
+++ xychan/atom.py
@@ -11,7 +11,7 @@ def atom_threads(board_name):
                .limit(50))
 
     posts = [thread.posts[0] for thread in threads]
-    feed_title = "Threads in /%s/ - %s" % (board.short_name, c.server_name)
+    feed_title = "Threads in %s - %s" % (board.pretty_title, c.server_name)
     feed_url = rurl('atom-threads', board_name=board_name)
     self_url = rurl('board', board_name=board.short_name)
     return locals()
@@ -27,8 +27,8 @@ def atom_thread_posts(board_name, thread_id):
              .order_by(desc(Post.id))
              .limit(50))
 
-    feed_title = "Posts in /%s/ thread %i - %s" % (
-        board.short_name, thread.id, c.server_name)
+    feed_title = "Posts in %s thread %i - %s" % (
+        board.pretty_title, thread.id, c.server_name)
     feed_url = rurl('atom-thread-posts', board_name=board.short_name,
                     thread_id=thread.id)
     self_url = rurl('thread', board_name=board.short_name, thread_id=thread.id)
